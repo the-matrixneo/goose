cd5fe6c77e90ce7773450a3520a5e1df57dfa1ba
Zane Staggs <zane@squareup.com>
2025-07-11 10:19:09 -0700

    Fix bg color for layout

--- ui/desktop/src/components/Layout/MainPanelLayout.tsx
+++ ui/desktop/src/components/Layout/MainPanelLayout.tsx
@@ -7,7 +7,9 @@ export const MainPanelLayout: React.FC<{
   return (
     <div className={`h-dvh`}>
       {/* Padding top matches the app toolbar drag area height - can be removed for full bleed */}
-      <div className={`flex flex-col bg-background-muted flex-1 min-w-0 h-full min-h-0 ${removeTopPadding ? '' : 'pt-[32px]'}`}>
+      <div
+        className={`flex flex-col bg-background-default flex-1 min-w-0 h-full min-h-0 ${removeTopPadding ? '' : 'pt-[32px]'}`}
+      >
         {children}
       </div>
     </div>
