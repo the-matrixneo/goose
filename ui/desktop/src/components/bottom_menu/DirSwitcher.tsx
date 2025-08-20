a3682210272d8cb8c6e9ecfd5a6ea864b84aa645
spencrmartin <spencermartin@squareup.com>
2025-08-20 08:54:55 -0400

    fix: completely remove Tooltip from DirSwitcher to prevent infinite update loop

--- ui/desktop/src/components/bottom_menu/DirSwitcher.tsx
+++ ui/desktop/src/components/bottom_menu/DirSwitcher.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { FolderDot } from 'lucide-react';
-import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '../ui/Tooltip';
 
 interface DirSwitcherProps {
   className?: string;
@@ -26,23 +25,15 @@ export const DirSwitcher: React.FC<DirSwitcherProps> = ({ className = '' }) => {
   };
 
   return (
-    <TooltipProvider>
-      <Tooltip>
-        <TooltipTrigger asChild>
-          <button
-            className={`z-[100] hover:cursor-pointer text-text-default/70 hover:text-text-default text-xs flex items-center transition-colors pl-1 [&>svg]:size-4 ${className}`}
-            onClick={handleDirectoryClick}
-          >
-            <FolderDot className="mr-1" size={16} />
-            <div className="max-w-[200px] truncate [direction:rtl]">
-              {workingDir}
-            </div>
-          </button>
-        </TooltipTrigger>
-        <TooltipContent side="top">
-          {workingDir}
-        </TooltipContent>
-      </Tooltip>
-    </TooltipProvider>
+    <button
+      className={`z-[100] hover:cursor-pointer text-text-default/70 hover:text-text-default text-xs flex items-center transition-colors pl-1 [&>svg]:size-4 ${className}`}
+      onClick={handleDirectoryClick}
+      title={workingDir} // Use native HTML tooltip instead
+    >
+      <FolderDot className="mr-1" size={16} />
+      <div className="max-w-[200px] truncate [direction:rtl]">
+        {workingDir}
+      </div>
+    </button>
   );
 };
