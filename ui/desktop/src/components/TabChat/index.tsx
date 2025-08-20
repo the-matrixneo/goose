38097fcef1a9c7f28fc150106b1011ec38240e35
spencrmartin <spencermartin@squareup.com>
2025-08-20 08:28:30 -0400

    fix: add FocusModeContext and wrap TabChatPair with provider

--- ui/desktop/src/components/TabChat/index.tsx
+++ ui/desktop/src/components/TabChat/index.tsx
@@ -1,6 +1,18 @@
-import TabBar from './TabBar';
-import TabPill from './TabPill';
-import TabChatManager from './TabChatManager';
+import React from 'react';
+import { FocusModeProvider } from '../../contexts/FocusModeContext';
 import TabChatPair from './TabChatPair';
 
-export { TabBar, TabPill, TabChatManager, TabChatPair };
+// Wrap TabChatPair with FocusModeProvider to fix the context error
+const TabChatPairWithFocusMode = (props) => {
+  return (
+    <FocusModeProvider>
+      <TabChatPair {...props} />
+    </FocusModeProvider>
+  );
+};
+
+export { TabChatPairWithFocusMode as TabChatPair };
+export { default as TabBar } from './TabBar';
+export { default as TabPill } from './TabPill';
+export { default as TabChatManager } from './TabChatManager';
+export { default } from './TabChatPair';
