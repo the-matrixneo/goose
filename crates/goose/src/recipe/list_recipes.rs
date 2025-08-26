ed77066dd56e0d23285af819db5194e7d3e4169e
Lifei Zhou <lifei@squareup.com>
2025-08-27 08:35:44 +1000

    list_recipe wip

--- crates/goose/src/recipe/list_recipes.rs
+++ crates/goose/src/recipe/list_recipes.rs
@@ -1,6 +1,16 @@
-use anyhow::Result;
+use std::path::PathBuf;
 
-pub fn list_recipes(recipe_dir: &str) -> Result<()> {
-    // read recipes from recipe_dir
+use anyhow::Result;
+use etcetera::{choose_app_strategy, AppStrategy};
+
+use crate::config::APP_STRATEGY;
+
+pub fn list_recipes() -> Result<()> {
+    let current_dir = std::env::current_dir()?;
+    let local_recipe_path = current_dir.join(".goose/recipes");
+    let global_recipe_path = choose_app_strategy(APP_STRATEGY.clone())
+            .map(|strategy| strategy.in_config_dir("recipes"))
+            .unwrap_or_else(|_| PathBuf::from("~/.config/goose/recipes"));
+    
     Ok(())
 }
